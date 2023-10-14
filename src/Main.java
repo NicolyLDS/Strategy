45c55c706fe72d41515aea0e228ec3497ee3036c
Nicoly Lopes <nicolylopes75@gmail.com>
2023-10-13 22:51:32 -0300

    Com Override

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,14 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Aritmetica calculo = new Aritmetica();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        //Geometrica calculo = new Geometrica();
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        Disciplina d = new Disciplina(calculo);
+        d.setNome("Padroes de Desenvolvimento");
+        d.setP1(10);
+        d.setP2(5);
+        d.CalcularMedia();
+        System.out.println(String.format("P1:%.2f P2:%.2f Media:%.2f Situacao: %s", d.getP1(), d.getP2(), d.getMedia(), d.getSituacao()));
     }
 }
